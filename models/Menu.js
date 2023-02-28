28cbe721df6f3ef3df0dede1312c8d97704a7ccc
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-02-28 12:24:06 -0500

    Update Menu.js

--- models/Menu.js
+++ models/Menu.js
@@ -2,5 +2,11 @@ const {sequelize} = require('../db');
 const { Sequelize } = require('sequelize');
 
 // TODO - create a Menu model
+const Menu = sequelize.define('Menu', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+  });
 
 module.exports = {Menu};
