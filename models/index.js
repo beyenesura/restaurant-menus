df496ec9fbe54921dfe2ce18e6905841b02628f5
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-02-28 18:23:17 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,4 +1,9 @@
 const {Restaurant} = require('./Restaurant')
 const {Menu} = require('./Menu')
-
+Menu.schema.add({
+  restaurant: {
+    type: Schema.Types.ObjectId,
+    ref: 'Restaurant'
+  }
+})
 module.exports = { Restaurant, Menu }
