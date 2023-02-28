017b4feb5241bf686ae6a04b0d8d26a81e072a55
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-02-28 18:27:49 -0500

    Update item.js

--- models/item.js
+++ models/item.js
@@ -1,1 +1,10 @@
+class Item {
+  constructor(name, image, price, vegetarian) {
+    this.name = name;
+    this.image = image;
+    this.price = price;
+    this.vegetarian = vegetarian;
+  }
+}
 
+export default Item;
