6c2021f9696b37a80b41bb4a67133e8144c445e1
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-02-28 12:45:47 -0500

    Update Restaurant.js

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,5 +2,19 @@ const {sequelize} = require('../db');
 const { Sequelize } = require('sequelize');
 
 // TODO - create a Restaurant model
+const Restaurant = sequelize.define('Restaurant', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cuisine: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
 
 module.exports = {Restaurant};
